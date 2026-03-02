3471908f1fbb618888b4d4ccb92f4f689d41824b
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-03-02 09:42:58 +0530

    Extract chat component styles into separate files

--- chatbot-project/src/components/ChatMessages.jsx
+++ chatbot-project/src/components/ChatMessages.jsx
@@ -1,10 +1,11 @@
 import { ChatMessage } from "./ChatMessage";
 import { useAutoScroll } from "../hooks/useAutoScroll";
+import './ChatMessages.css';
 
 export function ChatMessages({ chatMessages }) {
   const chatMessagesRef =  useAutoScroll(chatMessages);
   return (
-    <div className="chat-message-container" 
+    <div className="chat-messages-container" 
       ref={chatMessagesRef}>
       {chatMessages.map((chatMessage) => {
         return (
