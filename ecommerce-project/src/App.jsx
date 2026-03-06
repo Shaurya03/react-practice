30351268b49b64bbdd5369e801e054c606a6a091
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-03-06 17:23:38 +0530

    Setup react router and add homepage route

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,12 @@
-import { HomePage } from './pages/HomePage'
-import './App.css'
+import { Routes, Route } from 'react-router';
+import { HomePage } from './pages/HomePage';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+    </Routes>
   );
 }
 
