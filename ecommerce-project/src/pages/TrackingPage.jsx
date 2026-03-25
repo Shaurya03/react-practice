f99e0be74202d03d01b15953862de3fa1f01fb7b
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-03-25 17:59:04 +0530

    Fix incorrect casing causing delivery percent calculation issue

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -57,7 +57,7 @@ export function TrackingPage({ cart }) {
 
           <div className="delivery-date">
             {deliveryPercent >= 100 ? 'Delivered' : 'Arriving on'}
-            {dayjs(orderProduct.estimatedDeliverytimeMs).format('dddd, MMMM D')}
+            {dayjs(orderProduct.estimatedDeliveryTimeMs).format(' dddd, MMMM D')}
           </div>
 
           <div className="product-info">
