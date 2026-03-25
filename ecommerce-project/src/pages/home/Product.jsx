f764d942673730662669fa5968f837694bf4b9f8
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-03-25 18:46:07 +0530

    Display confirmation message when item is added to cart

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -6,6 +6,7 @@ import CheckmarkIcon from '../../assets/images/icons/checkmark.png';
 
 export function Product({ product, loadCart }) {
   const [quantity, setQuantity] = useState(1);
+  const [showAddedMessage, setShowAddedMessage] = useState(false);
 
   const addToCart = async () => {
     await axios.post('/api/cart-items', {
@@ -13,6 +14,12 @@ export function Product({ product, loadCart }) {
       quantity
     });
     await loadCart();
+
+    setShowAddedMessage(true);
+
+    setTimeout(() => {
+      setShowAddedMessage(false);
+    }, 2000);
   };
 
   const selectQuantity = (event) => {
@@ -62,7 +69,10 @@ export function Product({ product, loadCart }) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart">
+      <div className="added-to-cart"
+        style={{
+          opacity: showAddedMessage ? 1 : 0,
+        }}>
         <img src={CheckmarkIcon} />
         Added
       </div>
