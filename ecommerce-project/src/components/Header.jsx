11c8486859d3e98c76799f6eb05ee86eef327cbc
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-03-26 15:14:32 +0530

    Implement product search with URL sync and keyboard support

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { NavLink } from 'react-router';
+import { NavLink, useNavigate, useSearchParams } from 'react-router';
+import { useState } from 'react';
 import CartIcon from '../assets/images/icons/cart-icon.png';
 import SearchIcon from '../assets/images/icons/search-icon.png';
 import LogoWhite from '../assets/images/logo-white.png';
@@ -6,6 +7,27 @@ import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import './Header.css';
 
 export function Header({ cart }) {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const searchText = searchParams.get('search');
+
+  const [search, setSearch] = useState(searchText || '');
+
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchProducts = () => {
+    navigate(`/?search=${search}`);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      searchProducts();
+    }
+  };
+
   let totalQuantity = 0;
 
   (cart || []).forEach((cartItem) => {
@@ -25,9 +47,10 @@ export function Header({ cart }) {
         </div>
 
         <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
+          <input className="search-bar" type="text" placeholder="Search" 
+            value={search} onChange={updateSearchInput} onKeyDown={handleKeyDown} />
 
-          <button className="search-button">
+          <button className="search-button" onClick={searchProducts}>
             <img className="search-icon" src={SearchIcon} />
           </button>
         </div>
--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -1,21 +1,25 @@
 import axios from 'axios';
 import { Helmet } from 'react-helmet-async';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router';
 import { Header } from '../../components/Header';
 import { ProductsGrid } from './ProductsGrid';
 import './HomePage.css';
 
 export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search');
 
   useEffect(() => {
     const fetchHomeData = async () => {
-      const response = await axios.get('/api/products')
+      const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+      const response = await axios.get(urlPath);
       setProducts(response.data);
     };
     
     fetchHomeData();
-  }, []);
+  }, [search]);
   
   return (
     <>
