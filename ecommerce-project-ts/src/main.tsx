d25fc9a7f12fdb70f0c604cd97450409b372d7b6
Shaurya Chaudhary <shauryachaudhary2003@gmail.com>
2026-04-30 20:28:36 +0530

    Convert main.tsx and money utility to TypeScript

--- ecommerce-project-ts/src/main.tsx
+++ ecommerce-project-ts/src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import { HelmetProvider } from 'react-helmet-async';
-import App from './App.jsx';
+import App from './App.tsx';
 import './index.css';
 
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <HelmetProvider>
